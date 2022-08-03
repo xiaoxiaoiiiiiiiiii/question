747d9b203455284030e9cdbb9ef8d4445aa448e2
xiaoxiaoiiii <xiaoxiaoiiii@hotmail.com>
2022-08-03 12:17:39 +0800

    init Complete

---
+++ QuestionConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+程序由三个类（GameMessage、Game、Player）完成，使用了观察者模式。
+GameMessage类：消息类，Result属性表示需要传递的字符串格式的消息，
+Game类：可观察对象，暴露TakeAndNotify方法给Player调用，开放三个参数:
+        player：调用者
+        row：从哪一行拿取
+        count：拿取数量
+    TakeAndNotify方法调用逻辑：
+    1、通过Player的CanTake属性判断player本轮是否有拿取权，false状态，提醒player;
+    2、如果player有权拿取，判断拿取行为是否合规，由于业务比较简单，该部分没有做抽象；
+    3、合规的拿取行为,通过修改_items（池数据），更新数据状态。程序中池数据由Game的生命周期维护，生产环境可设计由外部Infrastructure层维护；
+    4、池数据和player的CanTake状态更新后，检查池数据中的item剩余数量，如果已清零，则分别提醒各player的输赢状态，完成后，提醒所有player游戏结束。
+Player类：观察者对象，通过JoinGame订阅Game。游戏完成后，通过Exit方法解除订阅，完成析构；
+    暴露TakeItems方法，通过调用Game对象的TakeAndNotify方法发送拿取请求；
+    暴露Name属性作为实例化对象的身份标识，此处未作排重处理，生产环境可设计唯一键属性（如，GUID id）用于排重；
+    暴露CanTake属性，用于拿取权检查，拿取状态由Game类维护。
+*/
+
+using System.Numerics;
+
+Game game = new Game();
+Player p1 = new Player("张三");
+Player p2 = new Player("李四");
+
+p1.JoinGame(game);
+p2.JoinGame(game);
+
+p1.TakeItems(1 , 1);
+p2.TakeItems(1 , 1);
+
+p1.TakeItems(0 , 1);
+p2.TakeItems(2 , 5);
+
+p1.TakeItems(0 , 1);
+p2.TakeItems(2 , 4);
+
+p2.TakeItems(2 , 4);
+p1.TakeItems(0 , 1);
+
+p2.TakeItems(0 , 1);
+p2.TakeItems(1 , 4);
+
+
+p1.Exit();
+p2.Exit();
+
+Console.ReadLine();
---
+++ QuestionConsole/question.cs
@@ -0,0 +1,174 @@
+﻿
+public class GameMessage
+{
+    public string Result { get; set; }
+}
+public class Game : IObservable<GameMessage>
+{
+    private List<IObserver<GameMessage>> Players;
+    private Dictionary<int , int> _items = new Dictionary<int , int>
+    {
+        { 0, 3 },
+        { 1, 5 },
+        { 2, 7 },
+    };
+
+    public Game ()
+    {
+        Players = new List<IObserver<GameMessage>>();
+    }
+
+    public IDisposable Subscribe (IObserver<GameMessage> player)
+    {
+        Players.Add(player);
+        return new UnSubscribe(Players , player);
+    }
+    private void ChangeCanTake (Player player)
+    {
+        Players.ForEach(s =>
+        {
+            if (s != player)
+            {
+                ((Player)s).CanTake = player.CanTake;
+            }
+        });
+        player.CanTake = !player.CanTake;
+
+    }
+    private void GameOver ()
+    {
+
+        foreach (var item in Players)
+        {
+            item.OnCompleted();
+        }
+    }
+    public void TakeAndNotify (Player player , int row , int count)
+    {
+        if (player.CanTake)
+        {
+            var rowitemcount = _items [ row ];
+            if (rowitemcount > 0 && count > 0)
+            {
+                if (rowitemcount >= count)
+                {
+                    _items [ row ] = rowitemcount - count;
+                    player.OnNext(new GameMessage { Result = $"hi {player.Name} You Take:{count} from {row} row, this row still leave {_items [ row ]}。" });
+                }
+                else
+                {
+                    //当前行拿取数量大于剩余数量，则全部拿走
+                    player.OnNext(new GameMessage { Result = $"hi {player.Name} You Take:{rowitemcount} from {row} row,no more items!" });
+                    _items [ row ] = 0;
+                }
+                ChangeCanTake(player);
+            }
+            else
+            {
+                if (rowitemcount == 0)
+                {
+                    player.OnNext(new GameMessage { Result = $"hi {player.Name} row: {row} no more items , please retry! " });
+                    return;
+                }
+                if (count <= 0)
+                {
+                    player.OnNext(new GameMessage { Result = $"hi {player.Name} don't cheat , please retry!" });
+                    return;
+                }
+            }
+
+
+
+            var sum = _items.AsQueryable().Count(s => s.Value > 0);
+
+            if (sum == 0)
+            {
+                Players.ForEach(s =>
+                {
+                    if ((Player)s != player)
+                    {
+                        s.OnNext(new GameMessage { Result = $"HI {((Player)s).Name} YOU ARE WiN!" });
+                    }
+                });
+
+                player.OnNext(new GameMessage { Result = $"HI {player.Name} YOU ARE LOST!" });
+
+                GameOver();
+            }
+        }
+        else
+        {
+            player.OnNext(new GameMessage { Result = $"hi {player.Name} It's not your turn!" });
+        }
+    }
+
+    public class UnSubscribe : IDisposable
+    {
+        List<IObserver<GameMessage>> _Players;
+        IObserver<GameMessage> _player;
+        public UnSubscribe (List<IObserver<GameMessage>> Players , IObserver<GameMessage> player)
+        {
+            _Players = Players;
+            _player = player;
+        }
+        public void Dispose ()
+        {
+            if (_Players.Contains(_player))
+            {
+                _Players.Remove(_player);
+            }
+        }
+    }
+}
+public class Player : IObserver<GameMessage>
+{
+    private Game.UnSubscribe exitor;
+    private Game _game;
+    public bool CanTake { get; set; } = true;
+    public Player (string name)
+    {
+        Name = name;
+    }
+
+    public void Exit ()
+    {
+        exitor.Dispose();
+        Console.WriteLine($"{Name} exit game!");
+    }
+    public void JoinGame (Game game)
+    {
+        if (_game == null)
+        {
+            exitor = (Game.UnSubscribe)game.Subscribe(this);
+            _game = game;
+
+            Console.WriteLine($"{Name} join game!");
+        }
+    }
+
+    public void TakeItems (int row , int count)
+    {
+        if (_game != null)
+        {
+            _game.TakeAndNotify(this , row , count);
+        }
+    }
+
+    public string Name { get; set; }
+
+    public void OnCompleted ()
+    {
+        _game = null;
+        Console.WriteLine($"Hi {Name}, Game Over!");
+    }
+
+    public void OnError (Exception error)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnNext (GameMessage GameMessage)
+    {
+        Console.WriteLine(GameMessage.Result);
+    }
+}
